bc402411e3f0d00be13db8bd30b45de34923a3bc
Kenneth <carnalkay@gmail.com>
2013-06-12 09:46:16 +0300

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -1,13 +1,19 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
+gem 'bootstrap-sass', '2.1'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
+  #gem 'pg', '0.12.2'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '1.2.0'
+  gem 'childprocess', '0.3.6'
+  gem 'spork', '0.9.2'
 end
 
 # Gems used only for assets and not required
@@ -42,6 +48,8 @@ gem 'jquery-rails' ,'2.0.2'
 
 group :test do
 	gem 'capybara', '1.1.2'
+  gem 'rb-fsevent', '0.9.1', :require => false
+  gem 'growl', '1.0.3'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,45 +1,70 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Twitlike" }
 
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      #visit '/static_pages/home'
+      visit home_path
       page.should have_content('Twitlike')
     end
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Twitlike | Home")
+    it "should have the base title" do
+      #visit '/static_pages/home'
+      visit home_path
+      page.should have_selector('title', :text => "#{base_title}")
     end
+
+    it "should not have a custom page title" do
+      #visit '/static_pages/home'
+      visit home_path
+      page.should_not have_selector('title' , :text => '| Home')
+      end
   end
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      #visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Twitlike | Help")
+      #visit '/static_pages/help'
+      visit help_path
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      #visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Twitlike | About Us")
+      #visit '/static_pages/about'
+      visit about_path
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      #visit '/static_pages/contact'
+      visit contact_path
+      page.should have_content('Contact Us')
+    end
 
+    it "should have the right title" do
+      #visit '/static_pages/contact'
+      visit contact_path
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end 
 end
